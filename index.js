0b22fc5bade25a0640f71a1d2a69e6899967dbba
Anup-Mishra <62417488+Anup-Mishra@users.noreply.github.com>
2021-04-30 16:59:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(compress());
+mongoose.set('useCreateIndex', true);
 app.use(methodOverride());
 app.use(cors());
 app.use(helmet());
